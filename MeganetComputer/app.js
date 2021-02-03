a428499ace357a237754e57437f6fce772e2d325
alexis <alexisrock20@gmail.com>
2021-02-02 19:58:20 -0500

    Creacion de modelo cliente de la api

--- MeganetComputer/app.js
+++ MeganetComputer/app.js
@@ -6,8 +6,22 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var clienteRouter = require('./Api Services/Cliente/ClienteRoutes');
+
 
 var app = express();
+
+
+var mongoose = require('mongoose');
+const { assert } = require('console');
+var mongoDB = 'mongodb://localhost/Meganet'
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB Connection error:'));
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,10 +36,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/api/Clientes', clienteRouter);
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
+
+
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
