305282c7fb6d29e27856d2f8afe4dbb2f5bf6ecd
alexis <alexisrock20@gmail.com>
2021-02-08 21:07:14 -0500

    creacion modelos productos, categorias, inventario y marcas

---
+++ MeganetComputer/Api Services/Categoria/CategoriaController.js
@@ -0,0 +1,43 @@
+var Categoria = require('./categoria');
+
+
+exports.Caterogia_list = function(req, res){
+    Categoria.find({}, function(err, categorias){
+        res.status(200).json({
+            categorias: categorias
+        });
+    });
+
+}
+
+
+exports.Categoria_create = function(req, res){
+    var categoria = new Categoria({nombreCategoria: req.body.nombreCategoria});
+    categoria.save(function(err){
+        if(err) res.status(400).json({error : err.message});
+        res.status(200).json(categoria);
+    });
+}
+
+exports.Categortia_update = function(req, res){
+    var datos_categoria = {nombreCategoria: req.body.Categoriareq};
+    Categoria.findById(req.body.id,datos_categoria, function(err){
+        if(err) res.status(400).json({error: err.message });
+
+        res.status(200).json({mensaje: "Datos actualizados correctamente"});
+    } );
+
+}
+
+exports.Categoria_delete = function(req, res){
+
+    Categoria.findByIdAndDelete(req.body.id, function(err){
+        if (err) {
+            res.status(400).json({error: err.message });
+        }
+        else{
+          
+        res.status(200).json({mensaje: "Categoria Eliminada"});
+        }
+    })
+}
