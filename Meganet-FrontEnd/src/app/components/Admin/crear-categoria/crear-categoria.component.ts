1f557283667a30e734bdd22a8fa9985df9355f3f
alexis <alexisrock20@gmail.com>
2021-04-09 18:26:44 -0500

    cambios admin

---
+++ Meganet-FrontEnd/src/app/components/Admin/crear-categoria/crear-categoria.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderMenuComponent} from '../../../shared/header-menu/header-menu.component'
+@Component({
+  selector: 'app-crear-categoria',
+  templateUrl: './crear-categoria.component.html',
+  styleUrls: ['./crear-categoria.component.css']
+})
+export class CrearCategoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
